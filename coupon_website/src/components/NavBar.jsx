dc62f8f96a5b37e5d96d25f69f2f37568ea6511a
noagedo <gedonoa@gmail.com>
2024-04-23 18:19:11 +0300

    opening the project

---
+++ coupon_website/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+export const NavBar = () => {
+    return <>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#Login">Login</a>
+            <a href="#WishList">WishList</a>
+            <a href="#Admin">Admin</a>
+            <div class="dropdown">
+                <a href="#Category" class="dropbtn">Category</a>
+                <div class="dropdown-content">
+                    <a href="#">Category 1</a>
+                    <a href="#">Category 2</a>
+                    <a href="#">Category 3</a>
+                </div>
+            </div>
+        </div>
+    </>
+}
